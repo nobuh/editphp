0c653c43b9fed685f394585060ea3979b295f217
nobuh <nobu.hatano@gmail.com>
2022-07-20 22:26:00 +0900

    step16 A timeout for read

---
+++ README.md
@@ -0,0 +1,8 @@
+# editphp
+
+[Build Your Own Text Editor](https://viewsourcecode.org/snaptoken/kilo/index.html) in PHP.
+
+## Requirements
+- Linux
+- stty
+- php8.1-intl
---
+++ edit.php
@@ -0,0 +1,50 @@
+<?php
+
+function enableRawMode(): void
+{
+    exec('stty -echo -icanon');
+    exec('stty -isig');     // disable CTRL-C, Z
+    exec('stty -ixon');     // disable CTRL-S, Q
+    exec('stty -iexten');   // disable CTRL-V, O
+    exec('stty -icrnl');    // fix CTRL-M
+    exec('stty -opost');
+    exec('stty -brkint -inpck -istrip');    // disable misc
+    exec('stty cs8');
+  
+    register_shutdown_function('disableRawMode');
+}
+
+function disableRawMode(): void
+{
+    exec('stty sane');
+}
+
+function main(): void 
+{
+    $stdin = fopen('php://stdin', 'r');
+
+    enableRawMode();
+
+    $dummy = null;
+    $arr = array($stdin);
+    if (stream_select($arr, $dummy, $dummy, 1) === false) {
+        \fwrite(STDERR, "Unable to set timeout on STDIN" . PHP_EOL);
+        exit(1);
+    }
+
+    while (1) {
+        $c = 0;
+        $c = fread($stdin, 1);
+        if (IntlChar::iscntrl($c)) {
+            printf("%d\r\n", ord($c));
+        } else {
+            printf("%d ('%s')\r\n", ord($c), $c);
+        }
+        if ($c === 'q') {
+            break;
+        }
+    }
+    exit(0);
+}
+
+main();
